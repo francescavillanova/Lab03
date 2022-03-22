aebbedc3b033cbb2794152fb1562496beff4f7a5
Francesca <Francesca@172.21.173.13>
2022-03-22 15:25:06 +0100

    Fine esercizio 1

--- src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,73 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
-package it.polito.tdp.spellchecker;
-
-import java.net.URL;
+package it.polito.tdp.spellchecker; 
+ 
+import java.net.URL; 
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 
-public class FXMLController {
-
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
-
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
-
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-
+import it.polito.tdp.spellchecker.model.Dictionary;
+import javafx.event.ActionEvent; 
+import javafx.fxml.FXML; 
+import javafx.scene.control.ComboBox; 
+import javafx.scene.control.Label; 
+import javafx.scene.control.TextArea; 
+ 
+public class FXMLController { 
+	
+	Dictionary model=new Dictionary();
+ 
+    @FXML 
+    private ResourceBundle resources; 
+ 
+    @FXML 
+    private URL location; 
+ 
+    @FXML 
+    private ComboBox<String> cmbLanguage; 
+ 
+    @FXML 
+    private TextArea txtCorr; 
+ 
+    @FXML 
+    private Label txtErrori; 
+ 
+    @FXML 
+    private Label txtTempo; 
+ 
+    @FXML 
+    private TextArea txtTesto; 
+ 
+    @FXML 
+    void check(ActionEvent event) { 
+        model.loadDictionary(cmbLanguage.getValue());  //questo get prende cosa scelgo nel menù a tendina (comboBox)
+        
+        txtCorr.setText(model.spellCheckText(txtTesto.getText()).toString());
+        txtErrori.setText(""+model.spellCheckText(txtTesto.getText()).size()); //"" per renderlo una stringa, guardo la dimensione dell'elenco di errori
+        txtTempo.setText(""+model.getTempo());
+    } 
+ 
+    @FXML 
+    void clear(ActionEvent event) { 
+    	txtTesto.clear();
+    	txtCorr.clear();
+    	txtTempo.setText("");
+    	txtErrori.setText("");
+ 
+    } 
+    
+    
+    public void setModel(Dictionary model) {
+      this.model=model;
     }
+ 
+    @FXML 
+    void initialize() { 
+        assert cmbLanguage != null : "fx:id=\"cmbLanguage\" was not injected: check your FXML file 'Scene.fxml'."; 
+        assert txtCorr != null : "fx:id=\"txtCorr\" was not injected: check your FXML file 'Scene.fxml'."; 
+        assert txtErrori != null : "fx:id=\"txtErrori\" was not injected: check your FXML file 'Scene.fxml'."; 
+        assert txtTempo != null : "fx:id=\"txtTempo\" was not injected: check your FXML file 'Scene.fxml'."; 
+        assert txtTesto != null : "fx:id=\"txtTesto\" was not injected: check your FXML file 'Scene.fxml'."; 
+ 
+        cmbLanguage.getItems().add("English");  
+        cmbLanguage.getItems().add("Italian");  
+    } 
+ 
 }
-
-
---
+++ src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -0,0 +1,53 @@
+package it.polito.tdp.spellchecker.model;
+
+import java.io.*;
+import java.util.*;
+
+public class Dictionary {
+	
+	Set<String> dizionario; //lo definisco qui per poterlo richiamare nel controller
+	Long tempo;
+	
+	public void loadDictionary(String language) {
+		dizionario=new HashSet<String>(); //inizializzandolo qui ogni volta che scelgo la lingua lo ricrea
+		String l;
+		if(language.equals("English")) 
+			l="C:\\Users\\Francesca\\Desktop\\POLI\\TECNICHE DI PROGRAMMAZIONE\\LAIBeclipse\\Lab03\\src\\main\\resources\\English.txt";  //mi serve il percorso del file
+		else
+			l="C:\\Users\\Francesca\\Desktop\\POLI\\TECNICHE DI PROGRAMMAZIONE\\LAIBeclipse\\Lab03\\src\\main\\resources\\Italian.txt";
+		
+		try {
+			FileReader fr = new FileReader(l);
+			BufferedReader br = new BufferedReader(fr);
+			String word;
+			while ((word = br.readLine()) != null) {
+			// Aggiungere parola alla struttura dati
+				dizionario.add(word);
+			}
+			br.close();
+			 } catch (IOException e){
+			System.out.println("Errore nella lettura del file");
+			}
+	}
+
+	public List<String> spellCheckText(String testo){
+		List<String> paroleErrate=new LinkedList<String>();
+		testo=testo.toLowerCase();
+		testo=testo.replaceAll("[.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]\"]", "");  //serve per togliere tutta la punteggiatura e sostituirla con vuoto ""
+		String [] parole=testo.split(" ");
+		
+		Long start=System.nanoTime();
+		for(String s: parole) {
+			if(!dizionario.contains(s))
+				paroleErrate.add(s);
+		}
+		Long end=System.nanoTime();
+		tempo=end-start;
+		
+		return paroleErrate;
+	}
+	
+	public long getTempo() {
+		return tempo;
+	}
+}
